392d0600e2de9369ac5ffa1cd054b0bd273f5d48
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2019-06-18 14:23:11 +0200

    should be faster

--- src/EntityImporter.php
+++ src/EntityImporter.php
@@ -65,37 +65,37 @@ class EntityImporter
 
         $this->idParser = new BasicEntityIdParser();
         $this->importUser = User::newFromId(0);
-	$this->batchSize = 10;
+        $this->batchSize = 10;
 
-	//create a new property for external links to Wikidata called Wikidata ID
-	try{
-		$entity = new Property( null, null, 'external-id' );
-		$entity->setLabel("en", "Wikidata ID");
-		$revision = $this->entityStore->saveEntity($entity,'Import entity',$this->importUser,EDIT_NEW);
-		echo "here";
-		$wikidataPropertyId = $revision->getEntity()->getId();
-		//print_r($localId);
-		$this->entityMappingStore->add(new PropertyId('P1000000'),$wikidataPropertyId);
-	} catch (\Exception $ex) {
-		$this->logger->info("Wikidata ID property already existing");
-		//$pattern = "/\|.*]]/";
-		//preg_match($pattern, $ex->getMessage(), $match);
-		//$this->idWikidataProperty = str_replace("]","",str_replace("|","",$match[0]));
-		$this->logger->error($ex->getMessage());
-	    }
-	
+        //create a new property for external links to Wikidata called Wikidata ID
+        try {
+            $entity = new Property(null, null, 'external-id');
+            $entity->setLabel("en", "Wikidata ID");
+            $revision = $this->entityStore->saveEntity($entity, 'Import entity', $this->importUser, EDIT_NEW);
+            echo "here";
+            $wikidataPropertyId = $revision->getEntity()->getId();
+            //print_r($localId);
+            $this->entityMappingStore->add(new PropertyId('P1000000'), $wikidataPropertyId);
+        } catch (\Exception $ex) {
+            $this->logger->info("Wikidata ID property already existing");
+            //$pattern = "/\|.*]]/";
+            //preg_match($pattern, $ex->getMessage(), $match);
+            //$this->idWikidataProperty = str_replace("]","",str_replace("|","",$match[0]));
+            $this->logger->error($ex->getMessage());
+        }
+
     }
 
     public function importEntities(array $ids, $importStatements = true)
     {
-	$batches = array_chunk($ids, $this->batchSize);
+        $batches = array_chunk($ids, $this->batchSize);
 
         $stashedEntities = array();
 
-	//print_r($batches);
+        //print_r($batches);
         foreach ($batches as $batch) {
-	    $batch_new = [];
-	    //search if the entity or property was already instered in the Wiki 
+            $batch_new = [];
+            //search if the entity or property was already instered in the Wiki
             foreach ($batch as $key => $id) {
                 $newId = NULL;
                 if (substr($id, 0, 1) == 'Q') {
@@ -106,15 +106,15 @@ class EntityImporter
                 }
                 if ($newId == NULL || $newId == '') {
                     array_push($batch_new, $id);
-		}
-		if ($importStatements == true){
-	            array_push($batch_new,$id);
-		}
-	    }
-	    //print_r($batches_new);
+                }
+                if ($importStatements == true) {
+                    array_push($batch_new, $id);
+                }
+            }
+            //print_r($batches_new);
             $entities = $this->apiEntityLookup->getEntities($batch_new);
-	    //print_r($entities);
-	    if ($entities) {
+            //print_r($entities);
+            if ($entities) {
                 $this->importBadgeItems($entities);
             } else {
                 if (count($batch_new) != 0) {
@@ -127,29 +127,17 @@ class EntityImporter
         if ($importStatements === true) {
             //$stashedEntities = array_merge($stashedEntities, $this->importBatch($batch));
             foreach ($stashedEntities as $entity) {
-		$referencedEntities = $this->getReferencedEntities($entity);
-                $this->importEntities($referencedEntities, false);
+                $localId = $this->entityMappingStore->getLocalId($entity->getId());
+                echo $this->statementsCountLookup->getStatementCount($localId);
+                if ($localId && $this->statementsCountLookup->getStatementCount($localId) < 2) {
+                    $referencedEntities = $this->getReferencedEntities($entity);
+                    $this->importEntities($referencedEntities, false);
 
-                $entity_new = $entity;
-                $statements_new = $entity->getStatements();
-                $uri = WikibaseRepo::getDefaultInstance()->getSettings()->getSetting('conceptBaseUri');
-                foreach ($statements_new as $key1 => $statement_new) {
-                    $snak_new = $statement_new->getMainSnak();
-                    if ($snak_new instanceof PropertyValueSnak) {
-                        $data_value_new = $snak_new->getDataValue();
-                        if ($data_value_new instanceof UnboundedQuantityValue) {
-                            $unit = $data_value_new->getUnit();
-                            if (strpos($unit, 'http://www.wikidata.org/entity/') !== false) {
-                                $id = str_replace("http://www.wikidata.org/entity/", "", $unit);
-                                $newid = $this->entityMappingStore->getLocalId(new ItemId($id));
-                                $data_value_new = new UnboundedQuantityValue($data_value_new->getAmount(), $uri . $newid);
-                                $snak_new = new PropertyValueSnak($snak_new->getPropertyId(), $data_value_new);
-                                $statement_new->setMainSnak($snak_new);
-                            }
-                        }
-                    }
-                    $snakList_new = $statement_new->getQualifiers();
-                    foreach ($snakList_new as $key2 => $snak_new) {
+                    $entity_new = $entity;
+                    $statements_new = $entity->getStatements();
+                    $uri = WikibaseRepo::getDefaultInstance()->getSettings()->getSetting('conceptBaseUri');
+                    foreach ($statements_new as $key1 => $statement_new) {
+                        $snak_new = $statement_new->getMainSnak();
                         if ($snak_new instanceof PropertyValueSnak) {
                             $data_value_new = $snak_new->getDataValue();
                             if ($data_value_new instanceof UnboundedQuantityValue) {
@@ -159,17 +147,28 @@ class EntityImporter
                                     $newid = $this->entityMappingStore->getLocalId(new ItemId($id));
                                     $data_value_new = new UnboundedQuantityValue($data_value_new->getAmount(), $uri . $newid);
                                     $snak_new = new PropertyValueSnak($snak_new->getPropertyId(), $data_value_new);
+                                    $statement_new->setMainSnak($snak_new);
                                 }
                             }
                         }
-                        $snakList_new[$key2] = $snak_new;
+                        $snakList_new = $statement_new->getQualifiers();
+                        foreach ($snakList_new as $key2 => $snak_new) {
+                            if ($snak_new instanceof PropertyValueSnak) {
+                                $data_value_new = $snak_new->getDataValue();
+                                if ($data_value_new instanceof UnboundedQuantityValue) {
+                                    $unit = $data_value_new->getUnit();
+                                    if (strpos($unit, 'http://www.wikidata.org/entity/') !== false) {
+                                        $id = str_replace("http://www.wikidata.org/entity/", "", $unit);
+                                        $newid = $this->entityMappingStore->getLocalId(new ItemId($id));
+                                        $data_value_new = new UnboundedQuantityValue($data_value_new->getAmount(), $uri . $newid);
+                                        $snak_new = new PropertyValueSnak($snak_new->getPropertyId(), $data_value_new);
+                                    }
+                                }
+                            }
+                            $snakList_new[$key2] = $snak_new;
+                        }
                     }
-		}
-		$entity->getStatements()->addStatement(new Statement(new PropertyValueSnak(new PropertyId('P1000000'), new StringValue( (string) $entity->getId() )),null,null,null));
-		
-		$localId = $this->entityMappingStore->getLocalId($entity->getId());
-		echo $this->statementsCountLookup->getStatementCount($localId);
-		if ($localId && $this->statementsCountLookup->getStatementCount($localId)<2) {
+                    $entity->getStatements()->addStatement(new Statement(new PropertyValueSnak(new PropertyId('P1000000'), new StringValue((string)$entity->getId())), null, null, null));
                     $this->statementsImporter->importStatements($entity_new);
                 } else {
                     $this->logger->info(
@@ -177,12 +176,13 @@ class EntityImporter
                     );
                 }
             }
-	}else {
-	    foreach ($stashedEntities as $entity) {
-		    $entity->setStatements(new StatementList());
-		    $entity->getStatements()->addStatement(new Statement(new PropertyValueSnak(new PropertyId('P1000000'), new StringValue( (string) $entity->getId() )),null,null,null));
-		    $this->statementsImporter->importStatements($entity); 
-	}}
+        } else {
+            foreach ($stashedEntities as $entity) {
+                $entity->setStatements(new StatementList());
+                $entity->getStatements()->addStatement(new Statement(new PropertyValueSnak(new PropertyId('P1000000'), new StringValue((string)$entity->getId())), null, null, null));
+                $this->statementsImporter->importStatements($entity);
+            }
+        }
     }
 
     private function importBatch(array $batch)
@@ -222,14 +222,14 @@ class EntityImporter
 
     private function createEntity(EntityDocument $entity)
     {
-	$wikidataId = $entity->getId();
+        $wikidataId = $entity->getId();
         $entity->setId(null);
 
         $entity->setStatements(new StatementList());
 
-	//Adds external link to wikidata
-	//$wikidataPropertyId = $this->entityMappingStore-> getLocalId(new PropertyId('P1000000'));
-	//$entity->getStatements()->addStatement(new Statement(new PropertyValueSnak(new PropertyId((string)$wikidataPropertyId), new StringValue( (string)$wikidataId )),null,null,null));
+        //Adds external link to wikidata
+        //$wikidataPropertyId = $this->entityMappingStore-> getLocalId(new PropertyId('P1000000'));
+        //$entity->getStatements()->addStatement(new Statement(new PropertyValueSnak(new PropertyId((string)$wikidataPropertyId), new StringValue( (string)$wikidataId )),null,null,null));
         if ($entity instanceof Item) {
             $siteLinkList = $this->badgeItemUpdater->replaceBadges($entity->getSiteLinkList());
             $entity->setSiteLinkList($siteLinkList);
